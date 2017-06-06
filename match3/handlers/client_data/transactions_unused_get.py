7497841bbbe349c8b01da7a12f9af724a614665e
Your Name <you@example.com>
2017-06-06 16:09:04 +0300

    add: used_transactions_count added;

--- match3/handlers/client_data/transactions_unused_get.py
+++ match3/handlers/client_data/transactions_unused_get.py
@@ -6,4 +6,6 @@ from match3.preload import preload
 
 @preload()
 def transactions_unused_get_handler(protocol, entry_point, world, data):
-    return BtlOk(simplejson.dumps({"result": "ok","unused_transactions": world.unused_transactions}))
+    return BtlOk(simplejson.dumps({"result": "ok",
+                                   "unused_transactions": world.unused_transactions,
+                                   "used_transactions_count": len(world.used_transactions)}))
